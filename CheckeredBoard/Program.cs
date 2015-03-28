71ba98da9ccba13f6d86b260a8bf09434e4fc02e
smilealdway <smile.al.d.way@gmail.com>
2015-03-28 13:37:45 -0400

    First
    Commit

---
+++ CheckeredBoard/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaurabhGuptaRepliconCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cols = 5;
+            int rows = 5;
+            int width = 5;
+            int height = 5;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    for (int k = 0; k < cols; k++)
+                    {
+                        for (int l = 0; l < width; l++)
+                        {
+                            char charToPrint = (i + k) % 2 == 0 ? 'x' : 'o';
+                            Console.Write(charToPrint);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
